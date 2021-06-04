35ddb45b6619397b1fac29fb335342c6b9ee5c17
Shivam Chamoli <shivamchamoli99@gmail.com>
2021-06-04 21:45:32 +0530

    Phase 1 completed
    Added all the necessary routes of home, about, compose,posts,contact and dynamic rendering of data is completed.

--- app.js
+++ app.js
@@ -2,26 +2,70 @@
  const express = require('express');
  const bodyParser= require('body-parser');
  const journel= require(__dirname+"/Journel.js");
+ const headingData= require(__dirname+"/headingData.js")
+ const _ = require('lodash');
 
  
 
 //Objects for storing journel blogs
-const journel1= new journel("Harry", "Potter");
+const journel1= new journel("Harry", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente molestias corrupti eum doloremque nemo facilis ipsam enim. Atque aut sit a at iure nisi facilis quo, quas est? Vitae, adipisci.");
 console.log(journel1.heading);
 var journelArray=[];
 journelArray.push(journel1);
+
+console.log(headingData.home[0])
+
  //getting module functions
  const app= express();
  app.set('view engine', 'ejs');
  app.use(express.static("public"))
  app.use(bodyParser.urlencoded({extended:true}));
 
-//Get Request
 
+//Get Requests
+// 1. home route
 app.get('/', function(req,res){
-    res.render("index", {journels : journelArray})
-
+    res.render("index", {journels : journelArray , heading : headingData.home[0] , text:headingData.about[1]});
 });
+
+// 2. about route
+app.get('/about', function(req,res){
+    res.render("about", {heading : headingData.about[0] , text:headingData.about[1]});
+});
+
+// 3. contact route
+app.get('/contact', function(req,res){
+    res.render("contact", {heading : headingData.contact[0], text:headingData.contact[1]});
+});
+
+// 4.compose route
+app.get('/compose', function(req,res){
+    res.render("compose");
+});
+
+// 5. posts route
+app.get('/posts/:param', function(req, res){
+    const param= _.lowerCase(req.params.param);
+    console.log(param);
+    let postReq= new journel("No post found","");
+    for(let i=0; i<journelArray.length;i++){
+        if(param=== _.lowerCase(journelArray[i].heading)){
+            console.log('Match Found');
+            postReq=journelArray[i];
+            break;
+        }
+    }
+    res.render("post", {postReq: postReq});
+    
+})
+
+//Post Requests
+app.post("/",function(req,res){
+    const newJournel = new journel(req.body.journelTitle, req.body.journelText);
+    console.log(newJournel);
+    journelArray.push(newJournel)
+    res.redirect("/");
+})
 
  //Starting The Server
  app.listen(3000,function(){
---
+++ headingData.js
@@ -0,0 +1,9 @@
+const homeH="Home | An Insight";
+const contactH="Contact Us";
+const aboutH="About Us"
+
+const text= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, magnam vel? Adipisci enim eos explicabo deleniti et asperiores rem accusamus similique, tempora dolores vel ex maiores a voluptatem eaque non!. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum sed modi vel. Iure placeat omnis odio praesentium unde amet minus ullam, exercitationem deserunt ipsa reiciendis a eligendi numquam non aut!.Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, magnam vel? Adipisci enim eos explicabo deleniti et asperiores rem accusamus similique, tempora dolores vel ex maiores a voluptatem eaque non!. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum sed modi vel. Iure placeat omnis odio praesentium unde amet minus ullam, exercitationem deserunt ipsa reiciendis a eligendi numquam non aut!."
+
+module.exports.home=[homeH,text];
+module.exports.contact=[contactH,text];
+module.exports.about=[aboutH,text];
